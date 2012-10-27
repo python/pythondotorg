73ba1136a5ed1f30764a24400376f21c2f9df4d9
Jacob Kaplan-Moss <jacob@jacobian.org>
2012-10-27 15:11:30 -0500

    It lives

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-pythondotorg
-============
+python.org
+==========
 
-pythondotorg
+The new python.org!
+
+Getting going
+-------------
+
+Requires :sparkles:Python 3.3!:sparkles: (`brew install python3`)
+
+You'll want a virtualenv. Python 3.3 actually includes virtualenv built-in,
+so you can do:
+
+    $ pyvenv-3.3 <env>
+    $ source <env>/bin/activate
+    (etc)
+
+But you can also use your existing virtualenv/wrapper:
+
+    $ mkvirtualenv --python=python3.3 <env>
+
+And then it's the standard:
+
+    $ pip install -r requirements.txt
+    $ ./manage.py syncdb
+
+This expects a local database named "python.org". If you need to change it:
+
+    $ export DATABASE_URL=postgres://user:pass@host:port/dbname
+
---
+++ pydotorg/settings.py
@@ -0,0 +1,95 @@
+import os
+import dj_database_url
+
+### Basic config
+
+BASE = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+DEBUG = TEMPLATE_DEBUG = True
+SITE_ID = 1
+SECRET_KEY = 'hu9h&&%j*tcj2o9!k2w%ao=fcw&$0z$)la$&8vl+s$4y%r946h'
+
+### Databases
+
+DATABASES = {
+    'default': dj_database_url.config(default='postgres:///python.org')
+}
+
+### Locale settings
+
+TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'en-us'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+### Files (media and static)
+
+MEDIA_ROOT = os.path.join(BASE, 'media')
+MEDIA_URL = '/m/'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/var/www/example.com/static/"
+STATIC_ROOT = os.path.join(BASE, 'static-root')
+STATIC_URL = '/static/'
+
+### Templates
+
+TEMPLATE_DIRS = [
+    os.path.join(BASE, 'templates')
+]
+
+### URLs, WSGI, middleware, etc.
+
+ROOT_URLCONF = 'pydotorg.urls'
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+WSGI_APPLICATION = 'pydotorg.wsgi.application'
+
+### Apps
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+)
+
+### Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
