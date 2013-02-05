bdbeb36bbce0f5d2ba9d26314bb6d6d91d32dfe2
Jacob Kaplan-Moss <jacob@jacobian.org>
2013-02-05 15:45:30 -0600

    Move the content-managed fields to a hidden fieldset in the admin.

--- cms/admin.py
+++ cms/admin.py
@@ -5,6 +5,21 @@ class ContentManageableModelAdmin(admin.ModelAdmin):
     Base ModelAdmin class for any model that uses ContentManageable.
     """
 
+    def save_model(self, request, obj, form, change):
+        """
+        Automatically set obj.creator = request.user when the model's created.
+        """
+        if not change:
+            obj.creator = request.user
+        return super().save_model(request, obj, form, change)
+
+    #
+    # Automatically insert created/updated/creator into various places in the
+    # admin options -- readonly fields, list_filter, etc. This is a bit extra-
+    # clever to make it easier for subclasses to just say `readonly_fields =
+    # ['foo']` and not worry about the superclass stuff.
+    #
+
     def get_readonly_fields(self, request, obj=None):
         fields = list(super().get_readonly_fields(request, obj))
         return fields + ['created', 'updated', 'creator']
@@ -17,7 +32,22 @@ class ContentManageableModelAdmin(admin.ModelAdmin):
         fields = list(super().get_list_display(request))
         return fields + ['created', 'updated']
 
-    def save_model(self, request, obj, form, change):
-        if not change:
-            obj.creator = request.user
-        return super().save_model(request, obj, form, change)
+    def get_fieldsets(self, request, obj=None):
+        """
+        Move the created/updated/creator fields to a fieldset of its own,
+        at the end, and collapsed.
+        """
+        # Remove created/updated/creator from any existing fieldsets. They'll
+        # be there if the child class didn't manually declare fieldsets.
+        fieldsets = super().get_fieldsets(request, obj)
+        for name, fieldset in fieldsets:
+            for f in ('created', 'udpated', 'creator'):
+                if f in fieldset['fields']:
+                    fieldset['fields'].remove(f)
+
+        # Now add these fields to a collapsed fieldset at the end.
+        # FIXME: better name than "CMS metadata", that sucks.
+        return fieldsets + [("CMS metadata", {
+            'fields': ['creator', 'created', 'updated'],
+            'classes': ('collapse',),
+        })]
