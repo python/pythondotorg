57b57b662bb1a4caf7084d28f9662e9326907b41
Ray Schamp <ray@projectevolution.com>
2013-02-06 19:17:16 -0500

    Add some real templates.

--- pydotorg/urls.py
+++ pydotorg/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name="home.html"), name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('pages.urls')),
 )
