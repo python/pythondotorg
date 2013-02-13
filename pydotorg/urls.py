eeb9c7ebea6c1ed35ce009bdea1fbab64d29bc70
Barry Menard <barry@projectevolution.com>
2013-02-13 11:50:51 -0500

    create templates for (primary section) landing pages
    Signed-off-by: Barry Menard <barry@projectevolution.com>

--- pydotorg/urls.py
+++ pydotorg/urls.py
@@ -5,7 +5,17 @@ from django.views.generic.base import TemplateView
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name="home.html"), name='home'),
+    url(r'^$', TemplateView.as_view(template_name="pages/home.html"), name='home'),
+    url(r'^about/$', TemplateView.as_view(template_name="pages/about.html"), name='about'),
+    url(r'^download/$', TemplateView.as_view(template_name="pages/download.html"), name='download'),
+    url(r'^documentation/$', TemplateView.as_view(template_name="pages/documentation.html"), name='documentation'),
+    url(r'^community/$', TemplateView.as_view(template_name="pages/community.html"), name='community'),
+    url(r'^success-stories/$', TemplateView.as_view(template_name="pages/success-stories.html"), name='success-stories'),
+    url(r'^blog/$', TemplateView.as_view(template_name="pages/blog.html"), name='blog'),
+    url(r'^events/$', TemplateView.as_view(template_name="pages/events.html"), name='events'),
+    
+    url(r'^inner/$', TemplateView.as_view(template_name="inner.html"), name='inner'),
+    
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('pages.urls')),
 )
