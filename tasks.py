f6a8c8e8043151f05ae8f383cecbf5257eb1967c
Jacob Kaplan-Moss <jacob@jacobian.org>
2013-03-08 14:33:14 -0600

    Added a "django" command to tasks.

--- tasks.py
+++ tasks.py
@@ -1,5 +1,21 @@
-from invoke import run, task
+import invoke
 
-@task
+env = {
+    'host': 'virt-l4es2w.psf.osuosl.org',
+    'python': '/srv/redesign.python.org/shared/env/bin/python',
+    'deploydir': '/srv/redesign.python.org/current',
+}
+
+def run(cmd, **kwargs):
+    """
+    Wrapper around invoke.run to automatically interpolate env.
+    """
+    return invoke.run(cmd.format(**dict(env, **kwargs)))
+
+@invoke.task
 def chef():
-    run('ssh virt-l4es2w.psf.osuosl.org sudo chef-client')
+    run('ssh {host} sudo chef-client')
+
+@invoke.task
+def django(c):
+    run('ssh {host} sudo {python} {deploydir}/manage.py {c} --settings pydotorg.settings.staging', c=c)
