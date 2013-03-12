6f2897e1527d7c09abb722d6d7aa5ac9134f1307
Jeff Triplett <jeff.triplett@gmail.com>
2013-03-11 22:08:21 -0500

    Added template_name to Page model and updated the view.

--- pages/admin.py
+++ pages/admin.py
@@ -6,6 +6,10 @@ from .models import Page
 
 class PageAdmin(ContentManageableModelAdmin):
     search_fields = ['title', 'path']
+    fieldsets = [
+        (None, {'fields': ('title', 'path', 'content', 'is_published')}),
+        ('Advanced options', {'classes': ('collapse',), 'fields': ('template_name',)}),
+    ]
 
 
 admin.site.register(Page, PageAdmin)
