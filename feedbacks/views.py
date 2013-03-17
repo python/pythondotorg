00c9d227b753bc4c6b5c4ab6f39d6437caee6384
Jacob Kaplan-Moss <jacob@jacobian.org>
2013-03-17 08:46:17 -0700

    Made feedback CSRF exempt.
    Serving a CSRF token on every page makes the site uncacheable. So disable
    CSRF for this view. This means that an outsider can in theory spoof
    feedback, but the motivation for such an attack is minimal at best, so I'm
    OK with it.

--- feedbacks/views.py
+++ feedbacks/views.py
@@ -1,4 +1,6 @@
 from django.core import urlresolvers
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import CreateView
 from django.views.generic.base import TemplateView
 
@@ -14,6 +16,14 @@ class FeedbackCreate(CreateView):
     model = Feedback
     form_class = FeedbackForm
     template_name = 'feedbacks/feedback_form.html'
+
+    # Since this view can get POSTed by any page, serving CSRF tokens on those
+    # pages means the whole site gets uncacheable. So we disable CSRF for this
+    # view. This means an outsider can spoof feedback, but that's probably OK -
+    # the ramifications of such an "attack" are minor at most.
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
     def get_success_url(self):
         return urlresolvers.reverse('feedback_complete')
