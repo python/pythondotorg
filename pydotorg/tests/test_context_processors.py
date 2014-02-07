0a2aa610206915b794e50c352411ff26ba3bdf3b
Flavio Curella <flavio.curella@gmail.com>
2014-02-07 16:35:30 -0600

    fixed wrong test

--- pydotorg/tests/test_context_processors.py
+++ pydotorg/tests/test_context_processors.py
@@ -11,7 +11,7 @@ class MockRequest(object):
 class TemplateProcessorsTestCase(TestCase):
     def test_url_name(self):
         mock_request = MockRequest(path='/inner/')
-        self.assertEqual({'URL_NAMESPACE': None, 'URL_NAME': 'inner'}, context_processors.url_name(mock_request))
+        self.assertEqual({'URL_NAMESPACE': '', 'URL_NAME': 'inner'}, context_processors.url_name(mock_request))
 
         mock_request = MockRequest(path='/events/')
         self.assertEqual({'URL_NAMESPACE': 'events', 'URL_NAME': 'events:event_list'}, context_processors.url_name(mock_request))
