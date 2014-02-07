3b1633edf8a079a1a5e92ff4463ea7e14b1dac7f
Flavio Curella <flavio.curella@gmail.com>
2014-02-07 10:37:31 -0600

    have linux do linux things

--- events/management/commands/import_ics_calendars.py
+++ events/management/commands/import_ics_calendars.py
@@ -1,10 +1,17 @@
 from django.core.management.base import NoArgsCommand
 from events.models import Calendar
-from filelock.filelock import FileLock
 
 
 class Command(NoArgsCommand):
+    """
+    Imports ICS calendars.
+    When used in cron jobs, it is advised to add file-locking by using the flock(1)
+    command. Eg::
+
+        flock -n import_ics_calendars.lock -c django-admin.py import_ics_calendars --settings=pydotorg.settings.local
+
+    """
+
     def handle_noargs(self, *args, **kwargs):
         calendars = Calendar.objects.filter(url__isnull=False)
-        with FileLock('calendars_import'):
-            [calendar.import_ics() for calendar in calendars]
+        [calendar.import_ics() for calendar in calendars]
