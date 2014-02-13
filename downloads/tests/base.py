1bfb25fab7a48fae04b23a69e7c0f933023597b7
Frank Wiles <frank@revsys.com>
2014-02-13 17:30:13 -0600

    Fixing tests

--- downloads/tests/base.py
+++ downloads/tests/base.py
@@ -28,14 +28,14 @@ class BaseDownloadTests(TestCase):
             release=self.release_275,
             name='Windows x86 MSI Installer (2.7.5)',
             description='Windows binary -- does not include source',
-            path='ftp/python/2.7.5/python-2.7.5.msi',
+            url='ftp/python/2.7.5/python-2.7.5.msi',
         )
         self.release_275_windows_64bit = ReleaseFile.objects.create(
             os=self.windows,
             release=self.release_275,
             name='Windows X86-64 MSI Installer (2.7.5)',
             description='Windows AMD64 / Intel 64 / X86-64 binary -- does not include source',
-            path='ftp/python/2.7.5/python-2.7.5.amd64.msi'
+            url='ftp/python/2.7.5/python-2.7.5.amd64.msi'
         )
 
         self.release_275_osx = ReleaseFile.objects.create(
@@ -43,7 +43,7 @@ class BaseDownloadTests(TestCase):
             release=self.release_275,
             name='Mac OSX 64-bit/32-bit',
             description='Mac OS X 10.6 and later',
-            path='ftp/python/2.7.5/python-2.7.5-macosx10.6.dmg',
+            url='ftp/python/2.7.5/python-2.7.5-macosx10.6.dmg',
         )
 
         self.release_275_linux = ReleaseFile.objects.create(
@@ -51,7 +51,7 @@ class BaseDownloadTests(TestCase):
             release=self.release_275,
             is_source=True,
             description='Gzipped source',
-            path='ftp/python/2.7.5/Python-2.7.5.tgz',
+            url='ftp/python/2.7.5/Python-2.7.5.tgz',
         )
 
         self.draft_release = Release.objects.create(
