ce303b28b02b0ba7ca96d525696c979767f514a2
Frank Wiles <frank@revsys.com>
2014-02-14 17:40:30 -0600

    Making box admin sort by label

--- boxes/admin.py
+++ boxes/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from cms.admin import ContentManageableModelAdmin
 from .models import Box
 
+
 class BoxAdmin(ContentManageableModelAdmin):
-    pass
+    ordering = ('label', )
+
 
 admin.site.register(Box, BoxAdmin)
