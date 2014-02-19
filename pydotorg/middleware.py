cae67c80962153a1551fb546c465750561208f5e
Frank Wiles <frank@revsys.com>
2014-02-19 07:57:03 -0600

    Ensuring admin is not cached

---
+++ pydotorg/middleware.py
@@ -0,0 +1,11 @@
+
+
+class AdminNoCaching(object):
+    """
+    Middleware to ensure the admin is not cached by Fastly or other caches
+    """
+
+    def process_response(self, request, response):
+        if request.path.startswith('/admin'):
+            response['Cache-Control'] = 'private'
+        return response
