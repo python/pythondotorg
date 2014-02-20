8a21bfa4b80f78172837e3698d8bf0906550426e
peterlandry <peter.landry@gmail.com>
2014-02-20 16:11:24 -0500

    Use py3k super()

--- pydotorg/views.py
+++ pydotorg/views.py
@@ -16,7 +16,7 @@ class IndexView(TemplateView):
     template_name = "python/index.html"
 
     def get_context_data(self, **kwargs):
-        context = super(IndexView, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         context['code_samples'] = CodeSample.objects.published()[:5]
         return context
 
