2aebbd2c144b8dde18db0af509613b17df6a734c
Flavio Curella <flavio.curella@gmail.com>
2014-02-20 13:03:58 +0100

    modified calendar importer to not generate dupe calendars due to mismatching slugs. Close #86

--- events/tests/test_importer.py
+++ events/tests/test_importer.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.conf import settings
 
 from events.importer import ICSImporter
+from events.models import Calendar
 
 CUR_DIR = os.path.dirname(__file__)
 EVENTS_CALENDAR = os.path.join(CUR_DIR, 'events.ics')
@@ -11,13 +12,16 @@ EVENTS_CALENDAR_URL = 'https://www.google.com/calendar/ical/j7gov1cmnqr9tvg14k62
 
 
 class EventsImporterTestCase(TestCase):
+    def setUp(self):
+        self.calendar = Calendar.objects.create(url=EVENTS_CALENDAR_URL)
+
     def test_injest(self):
-        importer = ICSImporter()
+        importer = ICSImporter(self.calendar)
         with open(EVENTS_CALENDAR) as fh:
             ical = fh.read()
         importer.parse(ical)
 
     @unittest.skipIf(settings.SKIP_NETWORK_TESTS, 'Network tests are disabled.')
     def test_url(self):
-        importer = ICSImporter()
-        importer.from_url(EVENTS_CALENDAR_URL)        
+        importer = ICSImporter(self.calendar)
+        importer.from_url()        
