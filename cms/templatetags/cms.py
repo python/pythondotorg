0367e4f00dc869076f7ad54b19be19fc35036831
peterlandry <peter.landry@gmail.com>
2014-02-21 18:24:09 -0500

    Use a more descriptive tag name, and push the "-" required for display formatting out of the date format string

--- cms/templatetags/cms.py
+++ cms/templatetags/cms.py
@@ -6,13 +6,13 @@ register = template.Library()
 
 
 @register.filter(is_safe=True)
-def responsive_date(date):
+def iso_time_tag(date):
     """ Returns an ISO date, with the year tagged in a say-no-more span.
 
     This allows the date representation to shrink to just MM-DD. """
-    date_templ = '<time datetime="{timestamp}">{month_day}<span class="say-no-more">{year}</span></time>'
+    date_templ = '<time datetime="{timestamp}">{month_day}<span class="say-no-more">-{year}</span></time>'
     return mark_safe(date_templ.format(
         timestamp=format(date, 'c'),
         month_day=format(date, 'm-d'),
-        year=format(date, '-Y')
+        year=format(date, 'Y')
     ))
