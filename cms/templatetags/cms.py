54f455e267e63e64a912296b280261a30f17f0b0
peterlandry <peter.landry@gmail.com>
2014-02-21 12:04:52 -0500

    Standardize on ISO representation for latest news and upcoming events.

---
+++ cms/templatetags/cms.py
@@ -0,0 +1,18 @@
+from django import template
+from django.utils.dateformat import format
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+
+@register.filter(is_safe=True)
+def responsive_date(date):
+    """ Returns an ISO date, with the year tagged in a say-no-more span.
+
+    This allows the date representation to shrink to just MM-DD. """
+    date_templ = '<time datetime="{timestamp}">{month_day}<span class="say-no-more">{year}</span></time>'
+    return mark_safe(date_templ.format(
+        timestamp=format(date, 'c'),
+        month_day=format(date, 'm-d'),
+        year=format(date, '-Y')
+    ))
