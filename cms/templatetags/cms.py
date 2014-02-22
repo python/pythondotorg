f7d792d684e6c74f4a3e508bc29bbe2bacc458f0
Peter Landry <peter.landry@gmail.com>
2014-02-21 19:29:47 -0500

    Fix order of date parts

--- cms/templatetags/cms.py
+++ cms/templatetags/cms.py
@@ -10,9 +10,10 @@ def iso_time_tag(date):
     """ Returns an ISO date, with the year tagged in a say-no-more span.
 
     This allows the date representation to shrink to just MM-DD. """
-    date_templ = '<time datetime="{timestamp}">{month_day}<span class="say-no-more">-{year}</span></time>'
+    date_templ = '<time datetime="{timestamp}"><span class="say-no-more">{year}-</span>{month}-{day}</time>'
     return mark_safe(date_templ.format(
         timestamp=format(date, 'c'),
-        month_day=format(date, 'm-d'),
-        year=format(date, 'Y')
+        month=format(date, 'm'),
+        day=format(date, 'd'),
+        year=format(date, 'Y'),
     ))
