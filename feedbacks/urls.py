5387a3bae148785a511d45cc7ec7e4da2a4e2206
Benjamin Peterson <benjamin@python.org>
2014-02-24 16:51:14 -0500

    remove feedback capability (issue #65)

--- feedbacks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from .views import FeedbackComplete, FeedbackCreate
-
-
-urlpatterns = patterns('',
-    url(r'^$', FeedbackCreate.as_view(), name='feedback_create'),
-    url(r'^complete/$', FeedbackComplete.as_view(), name='feedback_complete'),
-)
