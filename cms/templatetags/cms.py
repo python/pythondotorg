03b50a0aa0454babaa151431d9326dcae11350ba
Flavio Curella <flavio.curella@gmail.com>
2014-02-27 14:59:30 +0100

    refactored `iso_time_tag` from a filter to an inclusion tag

--- cms/templatetags/cms.py
+++ cms/templatetags/cms.py
@@ -1,19 +1,14 @@
 from django import template
 from django.utils.dateformat import format
-from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 
-@register.filter(is_safe=True)
+@register.inclusion_tag('cms/iso_time_tag.html')
 def iso_time_tag(date):
-    """ Returns an ISO date, with the year tagged in a say-no-more span.
-
-    This allows the date representation to shrink to just MM-DD. """
-    date_templ = '<time datetime="{timestamp}"><span class="say-no-more">{year}-</span>{month}-{day}</time>'
-    return mark_safe(date_templ.format(
-        timestamp=format(date, 'c'),
-        month=format(date, 'm'),
-        day=format(date, 'd'),
-        year=format(date, 'Y'),
-    ))
+    return {
+        'timestamp': format(date, 'c'),
+        'month': format(date, 'm'),
+        'day': format(date, 'd'),
+        'year': format(date, 'Y'),
+    }
