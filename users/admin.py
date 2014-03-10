cf1183437a8abe2440d8e6cbe79a294abf3054a5
Flavio Curella <flavio.curella@gmail.com>
2014-03-10 16:21:46 -0500

    list api keys by user. Close #272

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.forms import AdminPasswordChangeForm
 
+from tastypie.admin import ApiKeyInline
+from tastypie.models import ApiKey
+
 from .forms import UserCreationForm, UserChangeForm
 from .models import User, Membership
 
@@ -10,6 +13,7 @@ class UserAdmin(BaseUserAdmin):
     form = UserChangeForm
     add_form = UserCreationForm
     change_password_form = AdminPasswordChangeForm
+    inlines = BaseUserAdmin.inlines + [ApiKeyInline]
 
 
 class MembershipAdmin(admin.ModelAdmin):
@@ -21,5 +25,18 @@ class MembershipAdmin(admin.ModelAdmin):
     date_hierarchy = 'created'
 
 
+class ApiKeyAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created', )
+    date_hierarchy = 'created'
+
+
 admin.site.register(User, UserAdmin)
 admin.site.register(Membership, MembershipAdmin)
+
+try:
+    admin.site.unregister(ApiKey)
+except admin.sites.NotRegistered:
+    pass
+finally:
+    admin.site.register(ApiKey, ApiKeyAdmin)
+
