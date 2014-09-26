75719c9378aeb446c28dc90556e6159d96d4744d
Frank Wiles <frank@revsys.com>
2014-09-26 05:02:38 -0500

    Removing old PEP code

--- peps/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from . import views
-from .feeds import LatestPepEntries
-
-urlpatterns = patterns('',
-    url(r'^$', views.PepListView.as_view(), name='pep_list'),
-    url(r'^rss/latest/$', LatestPepEntries(), name='pep_rss'),
-)
