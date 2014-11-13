375869328566fab6fea16e72c4031788fb859963
Frank Wiles <frank@revsys.com>
2014-11-13 16:30:31 -0600

    Add Job Title to admin

--- jobs/admin.py
+++ jobs/admin.py
@@ -7,7 +7,7 @@ from cms.admin import NameSlugAdmin, ContentManageableModelAdmin
 class JobAdmin(ContentManageableModelAdmin):
     date_hierarchy = 'dt_start'
     filter_horizontal = ['job_types']
-    list_display = ['__str__', 'status', 'company', 'company_name']
+    list_display = ['__str__', 'job_title', 'status', 'company', 'company_name']
     list_filter = ['status', 'telecommuting']
     raw_id_fields = ['category', 'company']
 
