fe8066d42b93193c010fb7992d3f4d2b70bce084
Henrique Bastos <henrique@bastos.net>
2015-01-04 22:04:55 -0200

    Force manage.py runserver to accept any ip
    When using Vagrant you must pass 0.0.0.0:8000 to
    runserver. Otherwise the host machine web browser 
    can’t access the guest box.
    
    However if you forget the argument, it’s not
    obvious why your browser don’t show the site.
    
    So to avoid frustration, let’s force the argument.

--- manage.py
+++ manage.py
@@ -3,6 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
+    # Add argument needed (and easily forgotten) when using Vagrant.
+    if 'runserver' in sys.argv and len(sys.argv) == 2:
+        sys.argv.append('0.0.0.0:8000')
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pydotorg.settings.local")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
