05b65d7725caf56ab2f5d7dac8c41bc7ec880926
Henrique Bastos <henrique@bastos.net>
2015-01-04 22:05:31 -0200

    Fix haystack warning complaining about the ‘http’

--- pydotorg/settings/local.py
+++ pydotorg/settings/local.py
@@ -7,7 +7,7 @@ PYTHON_ORG_CONTENT_SVN_PATH='/Users/flavio/working_copies/beta.python.org/build/
 HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-        'URL': 'http://127.0.0.1:9200/',
+        'URL': '127.0.0.1:9200',
         'INDEX_NAME': 'haystack',
     },
 }
