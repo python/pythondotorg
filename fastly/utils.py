8df77244bda3dccbe72bb5a0702f0270b441fb06
Frank Wiles <frank@revsys.com>
2015-01-12 10:10:55 -0600

    Purge Fastly on Page Save
        - Fixes #480
        - Is a NOOP if DEBUG=True or FASTLY_API_KEY does not exist
        - Will create infrastructure ticket to set key in production

---
+++ fastly/utils.py
@@ -0,0 +1,22 @@
+import requests
+
+from django.conf import settings
+
+
+def purge_url(path):
+    """
+    Purge a Fastly.com URL given a path. path argument must begin with a slash
+    """
+    if settings.DEBUG:
+        return
+
+    api_key = getattr(settings, 'FASTLY_API_KEY', None)
+    if api_key:
+        response = requests.request(
+            'PURGE',
+            'https://www.python.org{}'.format(path),
+            headers={'Fastly-Key': api_key},
+        )
+        return response
+
+    return None
