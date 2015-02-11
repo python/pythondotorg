6e99553e7bdb2bb1bdd00b9f650fb71f0931c08f
Frank Wiles <frank@revsys.com>
2015-02-11 11:45:46 -0600

    Fixing Job login/create/review links in sidebars
        - Moved from Box to template due to the need to be dynamic
        - Adjusted text and links to be more clear
        - Data migration to remove old box to avoid future confusion
        - Fixes #564

---
+++ jobs/migrations/0003_auto_20150211_1738.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('jobs', '0002_auto_20150211_1634'),
+        ('boxes', '0001_initial'),
+    ]
+
+    operations = [
+    ]
