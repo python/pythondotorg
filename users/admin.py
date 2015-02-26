3652337407d92a2cfb6508a6e1d4951ab1abc8a5
Frank Wiles <frank@revsys.com>
2015-02-26 09:32:35 -0600

    Adding membership to user admin

--- users/admin.py
+++ users/admin.py
@@ -9,11 +9,16 @@ from .forms import UserCreationForm, UserChangeForm
 from .models import User, Membership
 
 
+class MembershipInline(admin.StackedInline):
+    model = Membership
+    extra = 0
+    readonly_fields = ('created', 'updated')
+
 class UserAdmin(BaseUserAdmin):
     form = UserChangeForm
     add_form = UserCreationForm
     change_password_form = AdminPasswordChangeForm
-    inlines = BaseUserAdmin.inlines + [ApiKeyInline]
+    inlines = BaseUserAdmin.inlines + [ApiKeyInline, MembershipInline]
 
 
 class MembershipAdmin(admin.ModelAdmin):
