e019f61addc347fac16fb5a7a4427c403876730f
Berker Peksag <berker.peksag@gmail.com>
2015-03-10 13:57:08 +0200

    Delete chef/

--- chef/cookbooks/pythondotorg/metadata.rb
+++
@@ -1,7 +0,0 @@
-maintainer       "Project Evolution"
-maintainer_email "peter@projectevolution.com"
-license          "All rights reserved"
-description      "Installs/Configures regis for vagrant"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
-depends 		 "database", ">= 1.1.0"
