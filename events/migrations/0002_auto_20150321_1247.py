f146b36ebf030ad1dee5e7f7ac2f4303f90a8833
Berker Peksag <berker.peksag@gmail.com>
2015-03-21 14:48:57 +0200

    Add missing migration for the "all_day" field.

---
+++ events/migrations/0002_auto_20150321_1247.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('events', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='occurringrule',
+            name='all_day',
+            field=models.BooleanField(default=False),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='recurringrule',
+            name='all_day',
+            field=models.BooleanField(default=False),
+            preserve_default=True,
+        ),
+    ]
