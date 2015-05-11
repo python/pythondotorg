12d09880517ef14bc788fc0080cdb3a9e00feec9
Berker Peksag <berker.peksag@gmail.com>
2015-05-11 12:11:39 +0300

    Add feed for PSF board meeting minutes.
    Closes #739.

---
+++ minutes/feeds.py
@@ -0,0 +1,19 @@
+from django.contrib.syndication.views import Feed
+from django.core.urlresolvers import reverse_lazy
+
+from .models import Minutes
+
+
+class MinutesFeed(Feed):
+    title = 'PSF Board Meeting Minutes Feed'
+    description = 'PSF Board Meeting Minutes'
+    link = reverse_lazy('minutes_list')
+
+    def items(self):
+        return Minutes.objects.latest()[:20]
+
+    def item_title(self, item):
+        return 'PSF Meeting Minutes for {}'.format(item.date)
+
+    def item_description(self, item):
+        return item.content
