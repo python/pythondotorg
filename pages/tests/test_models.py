4e70d5db10bd03892e7d8c5576e142679f5b1e8c
Berker Peksag <berker.peksag@gmail.com>
2016-08-05 13:17:50 +0300

    Add a test for docutils security
    Refs #977

--- pages/tests/test_models.py
+++ pages/tests/test_models.py
@@ -1,9 +1,9 @@
-import ddt
+import os
 import unittest
 
-from .base import BasePageTests
+import ddt
 
-from .. import admin     # coverage FTW
+from .base import BasePageTests
 from ..models import Page, PAGE_PATH_RE
 
 
@@ -21,6 +21,30 @@ class PageModelTests(BasePageTests):
     def test_get_absolute_url(self):
         one = Page.objects.create(title='Testing', path='test/one.html', content='foo')
         self.assertEqual('/test/one.html/', one.get_absolute_url())
+
+    def test_docutils_security(self):
+        # see issue #977 for details
+        content = """
+        first line
+
+        .. raw:: html
+
+           <strong>second</strong> line
+
+        .. include:: {content_ht}
+
+        fourth line
+        """
+        content_ht = os.path.join(
+            os.path.dirname(__file__), 'fake_svn_content_checkout', 'content.ht'
+        )
+        page = Page.objects.create(
+            title='Testing', content=content.format(content_ht=content_ht),
+        )
+        self.assertEqual(
+            page.content.rendered,
+            '<blockquote>\n<p>first line</p>\n<p>fourth line</p>\n</blockquote>\n'
+        )
 
 
 @ddt.ddt
