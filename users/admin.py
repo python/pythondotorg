ae12ae8abbfc7df99027a0d99c8ea81101295bde
Berker Peksag <berker.peksag@gmail.com>
2016-08-05 15:30:39 +0300

    Move export_csv() action to its file

--- users/admin.py
+++ users/admin.py
@@ -4,36 +4,8 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from tastypie.admin import ApiKeyInline
 from tastypie.models import ApiKey
 
+from .actions import export_csv
 from .models import User, Membership
-
-
-def export_csv(modeladmin, request, queryset):
-    import csv
-    from django.http import HttpResponse
-
-    membership_name = {
-        0: 'Basic', 1: 'Supporting', 2: 'Sponsor', 3: 'Managing',
-        4: 'Contributing', 5: 'Fellow'
-    }
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename=membership.csv'
-    fieldnames = [
-        'membership_type', 'creator', 'email_address', 'votes',
-        'last_vote_affirmation',
-    ]
-    writer = csv.DictWriter(response, fieldnames=fieldnames)
-    writer.writeheader()
-    for obj in queryset:
-        writer.writerow({
-            'membership_type': membership_name.get(obj.membership_type),
-            'creator': obj.creator,
-            'email_address': obj.email_address,
-            'votes': obj.votes,
-            'last_vote_affirmation': obj.last_vote_affirmation,
-        })
-    return response
-
-export_csv.short_description = 'Export CSV'
 
 
 class MembershipInline(admin.StackedInline):
