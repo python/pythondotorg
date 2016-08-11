35a5c274f8dfcd950cf35b1af15d20b878a746c0
Berker Peksag <berker.peksag@gmail.com>
2016-08-11 15:57:46 +0300

    Update PEP source links to use GitHub
    Fixes #983

--- peps/tests/test_converters.py
+++ peps/tests/test_converters.py
@@ -2,7 +2,9 @@ from django.test import TestCase
 from django.core.exceptions import ImproperlyConfigured
 from django.test.utils import override_settings
 
-from peps.converters import check_paths, get_pep0_page
+from peps.converters import check_paths, get_pep0_page, get_pep_page
+
+from . import FAKE_PEP_REPO
 
 
 class PEPConverterTests(TestCase):
@@ -15,3 +17,13 @@ class PEPConverterTests(TestCase):
 
         with self.assertRaises(ImproperlyConfigured):
             get_pep0_page()
+
+    @override_settings(PEP_REPO_PATH=FAKE_PEP_REPO)
+    def test_source_link(self):
+        pep = get_pep_page('0525')
+        self.assertEqual(pep.title, 'PEP 525 -- Asynchronous Generators')
+        self.assertIn(
+            'Source: <a href="https://github.com/python/peps/blob/master/'
+            'pep-0525.txt">https://github.com/python/peps/blob/master/pep-0525.txt</a>',
+            pep.content.rendered
+        )
