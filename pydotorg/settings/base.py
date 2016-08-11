76a461f03f066eaf91f8590dcd5502efd7a3b249
Berker Peksag <berker.peksag@gmail.com>
2016-08-11 20:00:42 +0300

    Invalidate user sessions after password update
    Fixes #984

--- pydotorg/settings/base.py
+++ pydotorg/settings/base.py
@@ -103,6 +103,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'waffle.middleware.WaffleMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
