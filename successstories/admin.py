a3e3af10bf5056f491c44e1340d8a30995dc0afe
Berker Peksag <berker.peksag@gmail.com>
2017-07-27 16:57:32 +0300

    Show story links in admin

--- successstories/admin.py
+++ successstories/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib import messages
+from django.utils.html import format_html
 
 from .models import Story, StoryCategory
 from cms.admin import ContentManageableModelAdmin, NameSlugAdmin
@@ -20,7 +21,7 @@ class StoryAdmin(ContentManageableModelAdmin):
 
     def get_list_display(self, request):
         fields = list(super().get_list_display(request))
-        return fields + ['is_published', 'featured', 'get_weight_display']
+        return fields + ['show_link', 'is_published', 'featured', 'get_weight_display']
 
     def save_model(self, request, obj, form, change):
         """ Alert user to weight inbalance situations """
@@ -29,6 +30,9 @@ class StoryAdmin(ContentManageableModelAdmin):
         if weight_total != 100:
             messages.warning(request, "Warning, Success Story Featured Weights do not total 100%")
 
+    def show_link(self, obj):
+        return format_html('<a href="{0}">\U0001F517</a>'.format(obj.get_absolute_url()))
+    show_link.short_description = 'View on site'
 
 admin.site.register(StoryCategory, StoryCategoryAdmin)
 admin.site.register(Story, StoryAdmin)
