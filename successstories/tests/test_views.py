9154bbb7c4fd96318b0de48ccf2db70b296e5514
Berker Peksag <berker.peksag@gmail.com>
2017-08-01 15:20:57 +0300

    Move StoryTemplateTagTests to test_templatetags

--- successstories/tests/test_views.py
+++ successstories/tests/test_views.py
@@ -1,6 +1,5 @@
 import re
 
-from django import template
 from django.conf import settings
 from django.core import mail
 from django.core.urlresolvers import reverse
@@ -120,30 +119,3 @@ class StoryViewTests(TestCase):
         self.assertContains(response, 'Please use a unique name.')
 
         del mail.outbox[:]
-
-
-class StoryTemplateTagTests(StoryTestCase):
-
-    def render(self, tmpl, **context):
-        t = template.Template(tmpl)
-        return t.render(template.Context(context))
-
-    def test_get_story_categories(self):
-        r = self.render('{% load successstories %}{% get_story_categories as story_categories %}{% for category in story_categories %}{{ category }}{% endfor %}')
-        self.assertEqual(r, self.category.name)
-
-    def test_get_stories_latest(self):
-        r = self.render('{% load successstories %}{% get_stories_latest as stories %}{% for story in stories %}{{ story }}{% endfor %}')
-        self.assertEqual(r, self.story1.name)
-
-    def test_get_stories_by_category(self):
-        r = self.render('{% load successstories %}{% get_stories_by_category category_slug="arts" as category_stories %}{% for story in category_stories %}{{ story }}{% endfor %}')
-        self.assertEqual(r, self.story1.name)
-
-    def test_get_stories_by_category_invalid(self):
-        r = self.render('{% load successstories %}{% get_stories_by_category category_slug="poop" as category_stories %}{% for story in category_stories %}{{ story }}{% endfor %}')
-        self.assertEqual(r, '')
-
-    def test_get_featured_story(self):
-        r = self.render('{% load successstories %}{% get_featured_story as story %}{{ story }}')
-        self.assertEqual(r, self.story1.name)
