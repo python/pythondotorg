95f3a4ad54478ddc1dd623fdfad57a9943d8b9e5
Berker Peksag <berker.peksag@gmail.com>
2017-09-19 03:22:50 +0300

    Set custom YUI_BINARY path

--- pydotorg/settings/pipeline.py
+++ pydotorg/settings/pipeline.py
@@ -67,6 +67,8 @@ PIPELINE = {
     # ),
     'CSS_COMPRESSOR': 'pipeline.compressors.yui.YUICompressor',
     'JS_COMPRESSOR': 'pipeline.compressors.yui.YUICompressor',
+    # It's yui-compressor no yuicompressor on some systems.
+    'YUI_BINARY': '/usr/bin/env yui-compressor',
     # 'SASS_BINARY': 'cd %s && exec /usr/bin/env sass'  % os.path.join(BASE, 'static'),
     # 'SASS_ARGUMENTS': '--quiet --compass --scss -I $(dirname $(dirname $(gem which susy)))/sass'
 }
