4c501bd52ce4126805ce0c3ee018ae77125d3abf
Mario Corchero <mcorcherojim@bloomberg.net>
2017-11-01 20:05:31 +0000

    Add basic fields

--- events/forms.py
+++ events/forms.py
@@ -2,6 +2,14 @@ from django import forms
 
 
 class EventForm(forms.Form):
-    name = forms.CharField()
-    message = forms.CharField(widget=forms.Textarea)
+    event_name = forms.CharField()
+    event_type = forms.CharField()
+    python_focus = forms.CharField()
+    expected_attendees = forms.CharField()
+    location = forms.CharField()
+    date_from = forms.CharField()
+    date_to = forms.CharField()
+    link = forms.URLField()
+    notes = forms.CharField(widget=forms.Textarea)
+
 
