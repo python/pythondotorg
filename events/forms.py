7f10dc405d9a210d4f3bb56c403f1368550044f5
Mario Corchero <mcorcherojim@bloomberg.net>
2017-11-01 21:17:18 +0000

    Add placeholders for further info

--- events/forms.py
+++ events/forms.py
@@ -2,14 +2,26 @@ from django import forms
 
 
 class EventForm(forms.Form):
-    event_name = forms.CharField()
-    event_type = forms.CharField()
-    python_focus = forms.CharField()
-    expected_attendees = forms.CharField()
-    location = forms.CharField()
+    event_name = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Name of the event (including the user group name for user group events)'
+    }))
+    event_type = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'conference, bar camp, sprint, user group meeting, etc.'
+    }))
+    python_focus = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Data analytics, Web Development, Country-wide conference, etc...'
+    }))
+    expected_attendees = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': '300+'
+    }))
+    location = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'IFEMA building, Madrid, Spain'
+    }))
     date_from = forms.DateField(widget=forms.SelectDateWidget())
     date_to = forms.DateField(widget=forms.SelectDateWidget())
-    recurrence = forms.CharField()
+    recurrence = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'None, every second Thursday, monthly, etc.'
+    }))
     link = forms.URLField()
     description = forms.CharField(widget=forms.Textarea)
 
