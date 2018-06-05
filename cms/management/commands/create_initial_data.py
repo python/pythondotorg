c76894e985f6f43ac7da713658c42186bdee07af
froessler <11539266+fdroessler@users.noreply.github.com>
2018-06-05 16:28:57 +0100

    Add --app-label flag to create_initial_data command (#1276)
    Fixes #1177

--- cms/management/commands/create_initial_data.py
+++ cms/management/commands/create_initial_data.py
@@ -10,9 +10,24 @@ class Command(BaseCommand):
 
     help = 'Create initial data by using factories.'
 
-    def collect_initial_data_functions(self):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--app-label',
+            dest='app_label',
+            help='Provide an app label to create app specific data (e.g. --app-label boxes)',
+        )
+
+    def collect_initial_data_functions(self, app_label):
         functions = {}
-        for app in apps.get_app_configs():
+        if app_label:
+            try:
+                app_list = [apps.get_app_config(app_label)]
+            except LookupError:
+                self.stdout.write(self.style.ERROR('The app label provided does not exist as an application.'))
+                return
+        else:
+            app_list = apps.get_app_configs()
+        for app in app_list:
             try:
                 factory_module = importlib.import_module('{}.factories'.format(app.name))
             except ImportError:
@@ -24,8 +39,18 @@ class Command(BaseCommand):
                         break
         return functions
 
+    def output(self, app_name, verbosity, *, done=False, result=False):
+        if verbosity > 0:
+            if done:
+                self.stdout.write(self.style.SUCCESS('DONE'))
+            else:
+                self.stdout.write('Creating initial data for {!r}... '.format(app_name), ending='')
+        if verbosity >= 2 and result:
+            pprint.pprint(result)
+
     def handle(self, **options):
         verbosity = options['verbosity']
+        app_label = options['app_label']
         msg = (
             'Note that this command won\'t cleanup the database before '
             'creating new data.\n'
@@ -34,26 +59,26 @@ class Command(BaseCommand):
         confirm = input(self.style.WARNING(msg))
         if confirm not in ('y', 'yes'):
             return
-        if verbosity > 0:
-            self.stdout.write('Creating initial data for \'sitetree\'... ', ending='')
-        try:
-            call_command('loaddata', 'sitetree_menus', '-v0')
-        except Exception as exc:
-            self.stdout.write(self.style.ERROR('{}: {}'.format(type(exc).__name__, exc)))
-        else:
-            if verbosity > 0:
-                self.stdout.write(self.style.SUCCESS('DONE'))
-        functions = self.collect_initial_data_functions()
+        # Collect relevant functions for data generation.
+        functions = self.collect_initial_data_functions(app_label)
+        if not functions:
+            return
+
+        if not app_label:
+            self.output('sitetree', verbosity)
+            try:
+                call_command('loaddata', 'sitetree_menus', '-v0')
+            except Exception as exc:
+                self.stdout.write(self.style.ERROR('{}: {}'.format(type(exc).__name__, exc)))
+            else:
+                self.output('sitetree', verbosity, done=True)
+
         for app_name, function in functions.items():
-            if verbosity > 0:
-                self.stdout.write('Creating initial data for {!r}... '.format(app_name), ending='')
+            self.output(app_name, verbosity)
             try:
                 result = function()
             except Exception as exc:
                 self.stdout.write(self.style.ERROR('{}: {}'.format(type(exc).__name__, exc)))
                 continue
             else:
-                if verbosity > 0:
-                    self.stdout.write(self.style.SUCCESS('DONE'))
-                if verbosity >= 2:
-                    pprint.pprint(result)
+                self.output(app_name, verbosity, done=True, result=result)
