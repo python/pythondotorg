927a2a8d241ecb7e59b51ec0aeb479ae7411896a
mozillazg <mozillazg101@gmail.com>
2018-06-08 23:18:33 +0800

    Fix the bug that the `dump_pep_pages` command was broken

--- peps/management/commands/dump_pep_pages.py
+++ peps/management/commands/dump_pep_pages.py
@@ -4,14 +4,14 @@ from django.core.management import BaseCommand
 from pages.models import Page
 
 
-class Command(BaeeCommand):
+class Command(BaseCommand):
     """
     Dump PEP related Pages as indented JSON
     """
     help = "Dump PEP related Pages as indented JSON"
 
     def handle(self, **options):
-        qs = Page.objects.filter(path__startswith='peps/')
+        qs = Page.objects.filter(path__startswith='dev/peps/')
 
         serializers.serialize(
             format='json',
