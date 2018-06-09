56b8707bcb6121d636177ad69b9d345c307405be
Huang Huang <mozillazg101@gmail.com>
2018-06-09 19:09:25 +0800

    Fix typos in dump_pep_pages command (#1283)
    * Base class of Command should read BaseCommand
    * URLs of PEP pages start with /dev/peps/

--- peps/tests/test_commands.py
+++ peps/tests/test_commands.py
@@ -1,7 +1,10 @@
+import io
+
 from bs4 import BeautifulSoup
 
 from django.test import TestCase, override_settings
 from django.conf import settings
+from django.core import serializers
 from django.core.management import call_command
 from django.core.exceptions import ImproperlyConfigured
 
@@ -33,3 +36,12 @@ class PEPManagementCommandTests(TestCase):
         img = Image.objects.get(page__path='dev/peps/pep-3001/')
         soup = BeautifulSoup(img.page.content.raw, 'lxml')
         self.assertIn(settings.MEDIA_URL, soup.find('img')['src'])
+
+    @override_settings(PEP_REPO_PATH=FAKE_PEP_REPO)
+    def test_dump_pep_pages(self):
+        call_command('generate_pep_pages')
+        stdout = io.StringIO()
+        call_command('dump_pep_pages', stdout=stdout)
+        output = stdout.getvalue()
+        result = list(serializers.deserialize('json', output))
+        self.assertGreater(len(result), 0)
