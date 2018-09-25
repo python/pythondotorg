c5464165e9ef8d9d97f5dd91f21cb45edd857d2a
Jaap Roes <jroes@leukeleu.nl>
2018-09-25 11:09:50 +0200

    Remove unused Contributor and Translation model

--- blogs/tests/test_models.py
+++ blogs/tests/test_models.py
@@ -1,18 +1,10 @@
 from django.test import TestCase
 from django.utils import timezone
 
-from ..models import Translation, BlogEntry, Feed
+from ..models import BlogEntry, Feed
 
 
 class BlogModelTest(TestCase):
-
-    def test_translation(self):
-        t = Translation.objects.create(
-            name='Swiss',
-            url='http://python.ch',
-        )
-        self.assertEqual(str(t), 'Swiss')
-        self.assertEqual(t.get_absolute_url(), t.url)
 
     def test_blog_entry(self):
         now = timezone.now()
