ed9115004bdef04426134caadaad544dd363d567
Jaap Roes <jaap.roes@gmail.com>
2018-09-26 10:38:03 +0200

    Simplify JobQueySet (#1344)
    * Remove redundant exact lookups. 'exact' is the default
      lookup type.
    * Remove unnecessary Q objects. Filters are 'and' by default.
    * Simplify active lookup by reusing the approved method.
    * Use exclude() instead of negated Q.

--- jobs/managers.py
+++ jobs/managers.py
@@ -1,6 +1,5 @@
 import datetime
 
-from django.db.models import Q
 from django.db.models.query import QuerySet
 from django.utils import timezone
 
@@ -37,51 +36,45 @@ class JobCategoryQuerySet(QuerySet):
 class JobQuerySet(QuerySet):
 
     def approved(self):
-        return self.filter(status__exact=self.model.STATUS_APPROVED)
+        return self.filter(status=self.model.STATUS_APPROVED)
 
     def archived(self):
-        return self.filter(status__exact=self.model.STATUS_ARCHIVED)
+        return self.filter(status=self.model.STATUS_ARCHIVED)
 
     def draft(self):
-        return self.filter(status__exact=self.model.STATUS_DRAFT)
+        return self.filter(status=self.model.STATUS_DRAFT)
 
     def expired(self):
-        return self.filter(status__exact=self.model.STATUS_EXPIRED)
+        return self.filter(status=self.model.STATUS_EXPIRED)
 
     def rejected(self):
-        return self.filter(status__exact=self.model.STATUS_REJECTED)
+        return self.filter(status=self.model.STATUS_REJECTED)
 
     def removed(self):
-        return self.filter(status__exact=self.model.STATUS_REMOVED)
+        return self.filter(status=self.model.STATUS_REMOVED)
 
     def featured(self):
         return self.filter(is_featured=True)
 
     def editable(self):
-        return self.exclude(status__exact=self.model.STATUS_APPROVED)
+        return self.exclude(status=self.model.STATUS_APPROVED)
 
     def review(self):
         review_threshold = timezone.now() - datetime.timedelta(days=120)
         return self.filter(
-            Q(status__exact=self.model.STATUS_REVIEW) &
-            Q(created__gte=review_threshold)
+            status=self.model.STATUS_REVIEW,
+            created__gte=review_threshold,
         ).order_by('created')
 
     def moderate(self):
-        return self.filter(
-            ~Q(status__exact=self.model.STATUS_REVIEW)
-        )
+        return self.exclude(status=self.model.STATUS_REVIEW)
 
     def visible(self):
         """
         Jobs that should be publicly visible on the website. They will have an
         approved status and be less than 90 days old
         """
-        now = timezone.now()
-        return self.filter(
-            Q(status__exact=self.model.STATUS_APPROVED) &
-            Q(expires__gte=now)
-        )
+        return self.approved().filter(expires__gte=timezone.now())
 
     def by(self, user):
-        return self.filter(Q(creator=user))
+        return self.filter(creator=user)
