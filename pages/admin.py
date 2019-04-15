a3584412c7a1b56bd9a0cb8e8dd3a9eeef49a272
Jaap Roes <jroes@leukeleu.nl>
2019-04-15 11:34:04 +0200

    Remove PageAdminImageFileWidget

--- pages/admin.py
+++ pages/admin.py
@@ -1,37 +1,17 @@
-from django.conf import settings
 from django.contrib import admin
-from django.db import models
-from django.utils.safestring import mark_safe
-
-from bs4 import BeautifulSoup
 
 from cms.admin import ContentManageableModelAdmin
 from .models import Page, Image, DocumentFile
-
-
-class PageAdminImageFileWidget(admin.widgets.AdminFileWidget):
-
-    def render(self, name, value, attrs=None):
-        content = super().render(name, value, attrs=None)
-        return content
 
 
 class ImageInlineAdmin(admin.StackedInline):
     model = Image
     extra = 1
 
-    formfield_overrides = {
-        models.ImageField: {'widget': PageAdminImageFileWidget},
-    }
-
 
 class DocumentFileInlineAdmin(admin.StackedInline):
     model = DocumentFile
     extra = 1
-
-    formfield_overrides = {
-        models.FileField: {'widget': PageAdminImageFileWidget},
-    }
 
 
 class PagePathFilter(admin.SimpleListFilter):
