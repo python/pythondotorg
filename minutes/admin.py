d0d9fd13fc4c09fcda3a7a75893b3d66d5488964
Bernardo Fontes <bernardoxhc@gmail.com>
2020-10-09 10:37:16 -0300

    Admin interface to manage meetings/concerns

--- minutes/admin.py
+++ minutes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Minutes
+from .models import Minutes, Meeting, ConcernRole, Concern, ConcernedParty, DEFAULT_MARKUP_TYPE
 from cms.admin import ContentManageableModelAdmin
 
 
@@ -15,3 +15,24 @@ class MinutesAdmin(ContentManageableModelAdmin):
     def get_list_display(self, request):
         fields = list(super().get_list_display(request))
         return fields + ['is_published']
+
+
+@admin.register(Meeting)
+class MeetingAdmin(admin.ModelAdmin):
+    list_display = ["title", "date"]
+
+
+class ConcernRoleInline(admin.TabularInline):
+    classes = ["collapse"]
+    model = ConcernRole
+    extra = 1
+
+
+@admin.register(Concern)
+class ConcernAdmin(admin.ModelAdmin):
+    inlines = [ConcernRoleInline]
+
+
+@admin.register(ConcernedParty)
+class ConcernedPartyAdmin(admin.ModelAdmin):
+    pass
