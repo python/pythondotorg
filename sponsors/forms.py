eccda1bb1c9b939744c47680415572a3c6ea7b4e
Bernardo Fontes <bernardoxhc@gmail.com>
2020-10-15 18:36:03 -0300

    Split benefits by programs

--- sponsors/forms.py
+++ sponsors/forms.py
@@ -1,6 +1,9 @@
 from django import forms
+from django.utils.text import slugify
+from django.utils.translation import ugettext_lazy as _
 
-from sponsors.models import SponsorshipBenefit, SponsorshipLevel
+from sponsors.models import SponsorshipBenefit, SponsorshipLevel, SponsorshipProgram
+
 
 
 class SponsorshiptBenefitsForm(forms.Form):
@@ -8,7 +11,27 @@ class SponsorshiptBenefitsForm(forms.Form):
         queryset=SponsorshipLevel.objects.all(),
         widget=forms.RadioSelect(), required=False,
     )
-    benefits = forms.ModelMultipleChoiceField(
-        queryset=SponsorshipBenefit.objects.select_related("program", "minimum_level"),
-        widget=forms.CheckboxSelectMultiple(), required=True,
-    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        benefits_qs = SponsorshipBenefit.objects.select_related("program", "minimum_level")
+        for program in SponsorshipProgram.objects.all():
+            slug = slugify(program.name).replace("-", "_")
+            self.fields[f"benefits_{slug}"] = forms.ModelMultipleChoiceField(
+                queryset=benefits_qs.filter(program=program),
+                widget=forms.CheckboxSelectMultiple(), required=False,
+                label=_(f"{program.name} Sponsorship Benefits")
+            )
+
+    @property
+    def benefits_programs(self):
+        return [f for f in self if f.name.startswith("benefits_")]
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        if not any([cleaned_data.get(bp.name) for bp in self.benefits_programs]):
+            raise forms.ValidationError(_("You have to pick a minimum number of benefits."))
+
+        return cleaned_data
--- sponsors/tests/test_forms.py
+++ sponsors/tests/test_forms.py
@@ -1,16 +1,41 @@
+from model_bakery import baker
 from django.test import TestCase
 
 from sponsors.forms import SponsorshiptBenefitsForm
+from sponsors.models import SponsorshipBenefit
 
 
 class SponsorshiptBenefitsFormTests(TestCase):
 
-    def test_required_fields(self):
-        required_fields = ["benefits"]
+    def setUp(self):
+        self.psf = baker.make("sponsors.SponsorshipProgram", name="PSF")
+        self.wk = baker.make("sponsors.SponsorshipProgram", name="Working Group")
+        self.program_1_benefits = baker.make(SponsorshipBenefit, program=self.psf, _quantity=3)
+        self.program_2_benefits = baker.make(SponsorshipBenefit, program=self.wk, _quantity=5)
 
+    def test_benefits_organized_by_program(self):
+        form = SponsorshiptBenefitsForm()
+
+        field1, field2 = sorted(form.benefits_programs, key=lambda f: f.name)
+
+        self.assertEqual("benefits_psf", field1.name)
+        self.assertEqual("PSF Sponsorship Benefits", field1.label)
+        choices = list(field1.field.choices)
+        self.assertEqual(len(self.program_1_benefits), len(choices))
+        for benefit in self.program_1_benefits:
+            self.assertIn(benefit.id, [c[0] for c in choices])
+
+        self.assertEqual("benefits_working_group", field2.name)
+        self.assertEqual("Working Group Sponsorship Benefits", field2.label)
+        choices = list(field2.field.choices)
+        self.assertEqual(len(self.program_2_benefits), len(choices))
+        for benefit in self.program_2_benefits:
+            self.assertIn(benefit.id, [c[0] for c in choices])
+
+    def test_invalidate_form_without_benefits(self):
         form = SponsorshiptBenefitsForm(data={})
-
         self.assertFalse(form.is_valid())
-        self.assertEqual(len(required_fields), len(form.errors))
-        for required in required_fields:
-            self.assertIn(required, form.errors)
+        self.assertIn("__all__", form.errors)
+
+        form = SponsorshiptBenefitsForm(data={"benefits_psf": [self.program_1_benefits[0].id]})
+        self.assertTrue(form.is_valid())
