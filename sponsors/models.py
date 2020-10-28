67e5ee631181ea71efd094237eb8a4332b37776e
Bernardo Fontes <bernardoxhc@gmail.com>
2020-10-28 15:35:08 -0300

    Initial sponsor application modeling

--- sponsors/models.py
+++ sponsors/models.py
@@ -156,6 +156,117 @@ class SponsorshipBenefit(OrderedModel):
         pass
 
 
+class SponsorInformation(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Sponsor name",
+        help_text="Name of the sponsor, for public display.",
+    )
+    description = models.TextField(
+        verbose_name="Sponsor description",
+        help_text="Brief description of the sponsor for public display.",
+    )
+    landing_page_url = models.URLField(
+        blank=True,
+        null=True,
+        verbose_name="Sponsor landing page",
+        help_text="Sponsor landing page URL. This may be provided by the sponsor, however the linked page may not contain any sales or marketing information.",
+    )
+    web_logo = models.ImageField(
+        upload_to="sponsor_web_logos",
+        verbose_name="Sponsor web logo",
+        help_text="For display on our sponsor webpage. High resolution PNG or JPG, smallest dimension no less than 256px",
+    )
+    print_logo = models.FileField(
+        upload_to="sponsor_print_logos",
+        blank=True,
+        null=True,
+        verbose_name="Sponsor print logo",
+        help_text="For printed materials, signage, and projection. SVG or EPS",
+    )
+
+    primary_phone = models.CharField("Sponsor Primary Phone", max_length=32)
+    mailing_address = models.TextField("Sponsor Mailing/Billing Address")
+
+
+class SponsorContact(models.Model):
+    sponsor = models.ForeignKey(
+        "SponsorInformation", on_delete=models.CASCADE, related_name="contacts"
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE
+    )  # Optionally related to a User! (This needs discussion)
+    primary = models.BooleanField(
+        default=False, help_text="If this is the primary contact for the sponsor"
+    )
+    manager = models.BooleanField(
+        default=False,
+        help_text="If this contact can manage sponsorship information on python.org",
+    )
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=256)
+    phone = models.CharField("Contact Phone", max_length=32)
+
+    # Sketch of something we'll need to determine if a user is able to make _changes_ to sponsorship benefits/logos/descriptons/etc.
+    @property
+    def can_manage(self):
+        if self.user is not None and (self.primary or self.manager):
+            return True
+
+
+class Sponsorship(models.Model):
+    sponsor_info = models.ForeignKey(
+        "SponsorInformation", null=True, on_delete=models.SET_NULL
+    )
+    applied_on = models.DateField(auto_now_add=True)
+    approved_on = models.DateField(null=True, blank=True)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+
+    level_name = models.CharField(max_length=64)
+    sponsorship_fee = models.PositiveIntegerField(null=True, blank=True)
+
+
+class SponsorBenefit(models.Model):
+    sponsorship = models.ForeignKey(
+        Sponsorship, on_delete=models.CASCADE, related_name="benefits"
+    )
+    sponsorship_benefit = models.ForeignKey(
+        SponsorshipBenefit,
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        help_text="Sponsorship Benefit this Sponsor Benefit came from",
+    )
+    name = models.CharField(
+        max_length=1024,
+        verbose_name="Benefit Name",
+        help_text="For display in the statement of work and sponsor dashboard.",
+    )
+    description = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name="Benefit Description",
+        help_text="For display in the statement of work and sponsor dashboard.",
+    )
+    program = models.ForeignKey(
+        SponsorshipProgram,
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        verbose_name="Sponsorship Program",
+        help_text="Which sponsorship program the benefit is associated with.",
+    )
+
+
+################################################################################
+# Honestly not sure if we want to keep this object as is, or consider
+# reimplementing from scratch. For the purposes of this work I'm just going to
+# work around it for the moment and we can consider deletion/replacement at a
+# later review
+################################################################################
+
+
 class Sponsor(ContentManageable):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     content = MarkupField(default_markup_type=DEFAULT_MARKUP_TYPE, blank=True)
