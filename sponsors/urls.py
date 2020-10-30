0cdb9e9070205dd5485b04c9e2518c8c9cc36257
Bernardo Fontes <bernardoxhc@gmail.com>
2020-10-30 08:59:04 -0300

    Simple template view to work as the final step

--- sponsors/urls.py
+++ sponsors/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.generic.base import TemplateView
 
 from . import views
 
@@ -9,6 +10,13 @@ urlpatterns = [
         r"^application/new$",
         views.NewSponsorshipApplicationView.as_view(),
         name="new_sponsorship_application",
+    ),
+    url(
+        r"^application/finished$",
+        TemplateView.as_view(
+            template_name="sponsors/sponsorship_application_finished.html"
+        ),
+        name="finish_sponsorship_application",
     ),
     url(
         r"^application$",
