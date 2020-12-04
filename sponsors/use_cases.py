47f13a383da8fd63f44f6abdb111d9171a843754
Bernardo Fontes <bernardoxhc@gmail.com>
2020-12-04 14:39:52 -0300

    Refactor use cases

--- sponsors/use_cases.py
+++ sponsors/use_cases.py
@@ -2,68 +2,56 @@ from sponsors.models import Sponsorship, StatementOfWork
 from sponsors import notifications
 
 
-class CreateSponsorshipApplicationUseCase:
+class BaseUseCaseWithNotifications:
+    notifications = []
+
     def __init__(self, notifications):
         self.notifications = notifications
 
-    def execute(self, user, sponsor, benefits, package=None, request=None):
-        sponsorship = Sponsorship.new(sponsor, benefits, package, submited_by=user)
-
+    def notify(self, **kwargs):
         for notification in self.notifications:
-            notification.notify(request=request, sponsorship=sponsorship)
-
-        return sponsorship
+            notification.notify(**kwargs)
 
     @classmethod
     def build(cls):
-        uc_notifications = [
-            notifications.AppliedSponsorshipNotificationToPSF(),
-            notifications.AppliedSponsorshipNotificationToSponsors(),
-        ]
-        return cls(uc_notifications)
+        return cls(cls.notifications)
 
 
-class RejectSponsorshipApplicationUseCase:
-    def __init__(self, notifications):
-        self.notifications = notifications
+class CreateSponsorshipApplicationUseCase(BaseUseCaseWithNotifications):
+    notifications = [
+        notifications.AppliedSponsorshipNotificationToPSF(),
+        notifications.AppliedSponsorshipNotificationToSponsors(),
+    ]
+
+    def execute(self, user, sponsor, benefits, package=None, request=None):
+        sponsorship = Sponsorship.new(sponsor, benefits, package, submited_by=user)
+        self.notify(sponsorship=sponsorship, request=request)
+        return sponsorship
+
+
+class RejectSponsorshipApplicationUseCase(BaseUseCaseWithNotifications):
+    notifications = [
+        notifications.RejectedSponsorshipNotificationToPSF(),
+        notifications.RejectedSponsorshipNotificationToSponsors(),
+    ]
 
     def execute(self, sponsorship, request=None):
         sponsorship.reject()
         sponsorship.save()
-
-        for notification in self.notifications:
-            notification.notify(request=request, sponsorship=sponsorship)
-
+        self.notify(request=request, sponsorship=sponsorship)
         return sponsorship
 
-    @classmethod
-    def build(cls):
-        uc_notifications = [
-            notifications.RejectedSponsorshipNotificationToPSF(),
-            notifications.RejectedSponsorshipNotificationToSponsors(),
-        ]
-        return cls(uc_notifications)
 
-
-class ApproveSponsorshipApplicationUseCase:
-    def __init__(self, notifications):
-        self.notifications = notifications
-
+class ApproveSponsorshipApplicationUseCase(BaseUseCaseWithNotifications):
     def execute(self, sponsorship, request=None):
         sponsorship.approve()
         sponsorship.save()
         statement_of_work = StatementOfWork.new(sponsorship)
 
-        for notification in self.notifications:
-            notification.notify(
-                request=request,
-                sponsorship=sponsorship,
-                statement_of_work=statement_of_work,
-            )
+        self.notify(
+            request=request,
+            sponsorship=sponsorship,
+            statement_of_work=statement_of_work,
+        )
 
         return sponsorship
-
-    @classmethod
-    def build(cls):
-        uc_notifications = []
-        return cls(uc_notifications)
