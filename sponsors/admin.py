1635f7f8d79de23246f31cf913688b9772627360
Bernardo Fontes <bernardoxhc@gmail.com>
2020-12-10 15:41:02 -0300

    Move admin views to a specific file

--- sponsors/admin.py
+++ sponsors/admin.py
@@ -1,12 +1,9 @@
 from ordered_model.admin import OrderedModelAdmin
-from markupfield_helpers.helpers import render_md
 
-from django.contrib import messages
-from django.urls import path, reverse
 from django.contrib import admin
 from django.contrib.humanize.templatetags.humanize import intcomma
+from django.urls import path
 from django.utils.html import mark_safe
-from django.shortcuts import get_object_or_404, render, redirect
 
 from .models import (
     SponsorshipPackage,
@@ -19,9 +16,8 @@ from .models import (
     LegalClause,
     StatementOfWork,
 )
-from sponsors import use_cases
+from sponsors import views_admin
 from sponsors.forms import SponsorshipReviewAdminForm
-from sponsors.exceptions import SponsorshipInvalidStatusException
 from cms.admin import ContentManageableModelAdmin
 
 
@@ -298,62 +294,10 @@ class SponsorshipAdmin(admin.ModelAdmin):
     get_sponsor_contacts.short_description = "Contacts"
 
     def reject_sponsorship_view(self, request, pk):
-        sponsorship = get_object_or_404(self.get_queryset(request), pk=pk)
-
-        if request.method.upper() == "POST" and request.POST.get("confirm") == "yes":
-            try:
-                use_case = use_cases.RejectSponsorshipApplicationUseCase.build()
-                use_case.execute(sponsorship)
-                self.message_user(
-                    request, "Sponsorship was rejected!", messages.SUCCESS
-                )
-            except SponsorshipInvalidStatusException as e:
-                self.message_user(request, str(e), messages.ERROR)
-
-            redirect_url = reverse(
-                "admin:sponsors_sponsorship_change", args=[sponsorship.pk]
-            )
-            return redirect(redirect_url)
-
-        context = {"sponsorship": sponsorship}
-        return render(
-            request, "sponsors/admin/reject_application.html", context=context
-        )
+        return views_admin.reject_sponsorship_view(self, request, pk)
 
     def approve_sponsorship_view(self, request, pk):
-        sponsorship = get_object_or_404(self.get_queryset(request), pk=pk)
-        initial = {
-            "level_name": sponsorship.level_name,
-            "start_date": sponsorship.start_date,
-            "end_date": sponsorship.end_date,
-            "sponsorship_fee": sponsorship.sponsorship_fee,
-        }
-
-        form = SponsorshipReviewAdminForm(initial=initial, force_required=True)
-
-        if request.method.upper() == "POST" and request.POST.get("confirm") == "yes":
-            form = SponsorshipReviewAdminForm(data=request.POST)
-            if form.is_valid():
-                kwargs = form.cleaned_data
-                kwargs["request"] = request
-                try:
-                    use_case = use_cases.ApproveSponsorshipApplicationUseCase.build()
-                    use_case.execute(sponsorship, **kwargs)
-                    self.message_user(
-                        request, "Sponsorship was approved!", messages.SUCCESS
-                    )
-                except SponsorshipInvalidStatusException as e:
-                    self.message_user(request, str(e), messages.ERROR)
-
-                redirect_url = reverse(
-                    "admin:sponsors_sponsorship_change", args=[sponsorship.pk]
-                )
-                return redirect(redirect_url)
-
-        context = {"sponsorship": sponsorship, "form": form}
-        return render(
-            request, "sponsors/admin/approve_application.html", context=context
-        )
+        return views_admin.approve_sponsorship_view(self, request, pk)
 
 
 @admin.register(LegalClause)
@@ -463,11 +407,4 @@ class StatementOfWorkModelAdmin(admin.ModelAdmin):
         return my_urls + urls
 
     def preview_statement_of_work_view(self, request, pk):
-        sow = get_object_or_404(self.get_queryset(request), pk=pk)
-        # footnotes only work if in same markdown text as the references
-        text = f"{sow.benefits_list.raw}\n\n**Legal Clauses**\n{sow.legal_clauses.raw}"
-        html = render_md(text)
-        ctx = {"sow": sow, "benefits_and_clauses": mark_safe(html)}
-        return render(
-            request, "sponsors/admin/preview-statement-of-work.html", context=ctx
-        )
+        return views_admin.preview_statement_of_work_view(self, request, pk)
