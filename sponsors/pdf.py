8b2e3bbdfa29a4e8b7acb060dcf329a9765cf730
Bernardo Fontes <bernardoxhc@gmail.com>
2020-12-11 18:28:29 -0300

    Create auxiliar function to render PDF document as bytes

--- sponsors/pdf.py
+++ sponsors/pdf.py
@@ -1,16 +1,32 @@
 """
 This module is a wrapper around django-easy-pdf so we can reuse code
 """
-from easy_pdf.rendering import render_to_pdf_response
+from easy_pdf.rendering import render_to_pdf_response, render_to_pdf
 
 from markupfield_helpers.helpers import render_md
 from django.utils.html import mark_safe
 
 
-def render_sow_to_pdf_response(request, sow, **context):
+def _sow_context(sow, **context):
     # footnotes only work if in same markdown text as the references
     text = f"{sow.benefits_list.raw}\n\n**Legal Clauses**\n{sow.legal_clauses.raw}"
     html = render_md(text)
-    context = {"sow": sow, "benefits_and_clauses": mark_safe(html)}
+    context.update(
+        {
+            "sow": sow,
+            "benefits_and_clauses": mark_safe(html),
+        }
+    )
+    return context
+
+
+def render_sow_to_pdf_response(request, sow, **context):
     template = "sponsors/admin/preview-statement-of-work.html"
+    context = _sow_context(sow, **context)
     return render_to_pdf_response(request, template, context)
+
+
+def render_sow_to_pdf_file(sow, **context):
+    template = "sponsors/admin/preview-statement-of-work.html"
+    context = _sow_context(sow, **context)
+    return render_to_pdf(template, context)
---
+++ sponsors/tests/test_pdf.py
@@ -0,0 +1,39 @@
+from unittest.mock import patch, Mock
+from model_bakery import baker
+from markupfield_helpers.helpers import render_md
+
+from django.http import HttpResponse, HttpRequest
+from django.template.loader import render_to_string
+from django.test import TestCase
+from django.utils.html import mark_safe
+
+from sponsors.pdf import render_sow_to_pdf_file, render_sow_to_pdf_response
+
+
+class TestRenderStatementOfWorkToPDF(TestCase):
+    def setUp(self):
+        self.sow = baker.make_recipe("sponsors.tests.empty_sow")
+        text = f"{self.sow.benefits_list.raw}\n\n**Legal Clauses**\n{self.sow.legal_clauses.raw}"
+        html = render_md(text)
+        self.context = {"sow": self.sow, "benefits_and_clauses": mark_safe(html)}
+        self.template = "sponsors/admin/preview-statement-of-work.html"
+
+    @patch("sponsors.pdf.render_to_pdf")
+    def test_render_pdf_using_django_easy_pdf(self, mock_render):
+        mock_render.return_value = "pdf content"
+
+        content = render_sow_to_pdf_file(self.sow)
+
+        self.assertEqual(content, "pdf content")
+        mock_render.assert_called_once_with(self.template, self.context)
+
+    @patch("sponsors.pdf.render_to_pdf_response")
+    def test_render_response_using_django_easy_pdf(self, mock_render):
+        response = Mock(HttpResponse)
+        mock_render.return_value = response
+
+        request = Mock(HttpRequest)
+        content = render_sow_to_pdf_response(request, self.sow)
+
+        self.assertEqual(content, response)
+        mock_render.assert_called_once_with(request, self.template, self.context)
