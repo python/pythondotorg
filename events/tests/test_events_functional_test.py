178742b4122d7f80f665cb8d798ddde0feddf900
Nwokolo Godwin <nwokolo.godwin.chidera@gmail.com>
2020-12-25 06:09:42 +0100

    Move All Test Data To Functional Test
    All test data concerning the provision of data to serve the functional tests have been moved to the functional test.
    As it improves readability.
    All other test data at test_views.py was reset
    to accommodate for the reduction in number of test data instances.

--- events/tests/test_events_functional_test.py
+++ events/tests/test_events_functional_test.py
@@ -1,18 +1,38 @@
-from django.test import LiveServerTestCase, TestCase
+import datetime
+
+from django.contrib.auth import get_user_model
+from django.test import LiveServerTestCase
 from django.utils import timezone
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver import Chrome
 
-from .test_views import EventsViewsTests
-from ..models import Event
+from ..models import Event, OccurringRule, Calendar
 
 
-class EventsPageFunctionalTests(LiveServerTestCase, TestCase):
+class EventsPageFunctionalTests(LiveServerTestCase):
     @classmethod
     def setUpClass(cls):
-        EventsViewsTests().setUpTestData()
-        super().setUpClass()
         cls.now = timezone.now()
+        cls.user = get_user_model().objects.create_user(username='username', password='password')
+        cls.calendar = Calendar.objects.create(creator=cls.user, slug="test-calendar-2")
+        cls.event_future_start_following_year = Event.objects.create(title='Event Starts Following Year',
+                                                                     creator=cls.user, calendar=cls.calendar)
+        cls.event_future_end_following_year = Event.objects.create(title='Event Ends Following Year',
+                                                                   creator=cls.user, calendar=cls.calendar)
+        recurring_time_dtstart = cls.now + datetime.timedelta(days=3)
+        recurring_time_dtend = recurring_time_dtstart + datetime.timedelta(days=5)
+
+        cls.rule_future_start_year = OccurringRule.objects.create(
+            event=cls.event_future_start_following_year,
+            dt_start=recurring_time_dtstart + datetime.timedelta(weeks=52),
+            dt_end=recurring_time_dtstart + datetime.timedelta(weeks=53),
+        )
+        cls.rule_future_end_year = OccurringRule.objects.create(
+            event=cls.event_future_end_following_year,
+            dt_start=recurring_time_dtstart,
+            dt_end=recurring_time_dtend + datetime.timedelta(weeks=52)
+        )
+        super(EventsPageFunctionalTests, cls).setUpClass()
 
     def setUp(self) -> None:
         try:
@@ -25,17 +45,18 @@ class EventsPageFunctionalTests(LiveServerTestCase, TestCase):
             webdriver.Firefox(options=self.browser)
 
     def tearDown(self) -> None:
-        self.browser.quit()
+        super().tearDown()
+        try:
+            self.browser.quit()
+        except Exception:
+            pass
 
-    def test_event_starting_future_year_displays_year(self):
-        event = Event.objects.get(title=f"Event Starts Following Year")
+    def test_event_starting_and_ending_future_year_displays_year(self):
+        event = self.event_future_start_following_year
         self.browser.get(self.live_server_url + '/events/')
         future_event_span_value = self.browser.find_element_by_id(str(event.id))
         self.assertIn(str(event.next_time.dt_start.year), future_event_span_value.text)
 
-    def test_event_ending_future_year_displays_year(self):
-        event = Event.objects.get(title=f"Event Ends Following Year")
-        self.browser.get(self.live_server_url + '/events/')
-        future_event_span_value = self.browser.find_element_by_id(str(event.id))
-        self.assertIn(str(event.next_time.dt_end.year), future_event_span_value.text)
-
+        event_2 = Event.objects.get(title="Event Ends Following Year")
+        future_event_span_value = self.browser.find_element_by_id(str(event_2.id))
+        self.assertIn(str(event_2.next_time.dt_end.year), future_event_span_value.text)
