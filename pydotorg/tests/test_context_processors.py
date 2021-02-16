144da0691276cdedc1faf5eabce08a1a9232ca5a
Bernardo Fontes <bernardoxhc@gmail.com>
2021-02-16 10:18:37 -0300

    Refactor tests to use django testing tools instead of using mock

--- pydotorg/tests/test_context_processors.py
+++ pydotorg/tests/test_context_processors.py
@@ -1,32 +1,28 @@
 from django.conf import settings
 from pydotorg import context_processors
-from django.test import TestCase
-
-
-class MockRequest:
-    def __init__(self, path):
-        self.path = path
-        super().__init__()
+from django.test import TestCase, RequestFactory
 
 
 class TemplateProcessorsTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
     def test_url_name(self):
-        mock_request = MockRequest(path='/inner/')
-        self.assertEqual({'URL_NAMESPACE': '', 'URL_NAME': 'inner'}, context_processors.url_name(mock_request))
+        request = self.factory.get('/inner/')
+        self.assertEqual({'URL_NAMESPACE': '', 'URL_NAME': 'inner'}, context_processors.url_name(request))
 
-        mock_request = MockRequest(path='/events/calendars/')
-        self.assertEqual({'URL_NAMESPACE': 'events', 'URL_NAME': 'events:calendar_list'}, context_processors.url_name(mock_request))
+        request = self.factory.get('/events/calendars/')
+        self.assertEqual({'URL_NAMESPACE': 'events', 'URL_NAME': 'events:calendar_list'}, context_processors.url_name(request))
 
-        mock_request = MockRequest(path='/getit-404/releases/3.3.3/not-an-actual-thing/')
-        self.assertEqual({}, context_processors.url_name(mock_request))
+        request = self.factory.get('/getit-404/releases/3.3.3/not-an-actual-thing/')
+        self.assertEqual({}, context_processors.url_name(request))
 
-        mock_request = MockRequest(path='/getit-404/releases/3.3.3/\r\n/')
-        self.assertEqual({}, context_processors.url_name(mock_request))
+        request = self.factory.get('/getit-404/releases/3.3.3/\r\n/')
+        self.assertEqual({}, context_processors.url_name(request))
 
-        mock_request = MockRequest(path='/nothing/here/')
-        self.assertEqual({}, context_processors.url_name(mock_request))
+        request = self.factory.get('/nothing/here/')
+        self.assertEqual({}, context_processors.url_name(request))
 
     def test_blog_url(self):
-        mock_request = MockRequest(path='/about/')
-        self.assertEqual({'BLOG_URL': settings.PYTHON_BLOG_URL}, context_processors.blog_url(mock_request))
-
+        request = self.factory.get('/about/')
+        self.assertEqual({'BLOG_URL': settings.PYTHON_BLOG_URL}, context_processors.blog_url(request))
