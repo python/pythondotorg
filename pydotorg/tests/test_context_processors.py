790d4014b0fa55703051f592e28f1c09dd4a9051
Bernardo Fontes <bernardoxhc@gmail.com>
2021-02-16 11:07:59 -0300

    Create context processor to dinamically build user's navbar

--- pydotorg/tests/test_context_processors.py
+++ pydotorg/tests/test_context_processors.py
@@ -1,6 +1,10 @@
+from model_bakery import baker
+
+from django.urls import reverse
 from django.conf import settings
 from pydotorg import context_processors
 from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import AnonymousUser
 
 
 class TemplateProcessorsTestCase(TestCase):
@@ -26,3 +30,64 @@ class TemplateProcessorsTestCase(TestCase):
     def test_blog_url(self):
         request = self.factory.get('/about/')
         self.assertEqual({'BLOG_URL': settings.PYTHON_BLOG_URL}, context_processors.blog_url(request))
+
+    def test_user_nav_bar_links_for_non_psf_members(self):
+        request = self.factory.get('/about/')
+        request.user = baker.make(settings.AUTH_USER_MODEL, username='foo')
+
+        expected_nav = {
+            "account": {
+                "label": "Your Account",
+                "urls": [
+                    {"url": reverse("users:user_detail", args=['foo']), "label": "View profile"},
+                    {"url": reverse("users:user_profile_edit"), "label": "Edit profile"},
+                    {"url": reverse("account_change_password"), "label": "Change password"},
+                ],
+            },
+            "psf_membership": {
+                "label": "Membership",
+                "urls": [
+                    {"url": reverse("users:user_nominations_view"), "label": "Nominations"},
+                    {"url": reverse("users:user_membership_create"), "label": "Become a PSF member"},
+                ],
+            }
+        }
+
+        self.assertEqual(
+            {"USER_NAV_BAR": expected_nav},
+            context_processors.user_nav_bar_links(request)
+        )
+
+    def test_user_nav_bar_links_for_psf_members(self):
+        request = self.factory.get('/about/')
+        request.user = baker.make(settings.AUTH_USER_MODEL, username='foo')
+        baker.make('users.Membership', creator=request.user)
+
+        expected_nav = {
+            "account": {
+                "label": "Your Account",
+                "urls": [
+                    {"url": reverse("users:user_detail", args=['foo']), "label": "View profile"},
+                    {"url": reverse("users:user_profile_edit"), "label": "Edit profile"},
+                    {"url": reverse("account_change_password"), "label": "Change password"},
+                ],
+            },
+            "psf_membership": {
+                "label": "Membership",
+                "urls": [
+                    {"url": reverse("users:user_nominations_view"), "label": "Nominations"},
+                    {"url": reverse("users:user_membership_edit"), "label": "Edit PSF membership"},
+                ],
+            }
+        }
+
+        self.assertEqual(
+            {"USER_NAV_BAR": expected_nav},
+            context_processors.user_nav_bar_links(request)
+        )
+
+    def test_user_nav_bar_links_for_anonymous_user(self):
+        request = self.factory.get('/about/')
+        request.user = AnonymousUser()
+
+        self.assertEqual({"USER_NAV_BAR": {}}, context_processors.user_nav_bar_links(request))
