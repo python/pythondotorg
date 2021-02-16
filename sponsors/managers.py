ed77dd096c1d826ac6ca1287587235192ea33a47
Bernardo Fontes <bernardoxhc@gmail.com>
2021-02-16 11:43:20 -0300

    Querset method and property shortcut to list user's visible sponsorships

--- sponsors/managers.py
+++ sponsors/managers.py
@@ -1,3 +1,4 @@
+from django.db.models import Q, Subquery
 from django.db.models.query import QuerySet
 
 
@@ -5,3 +6,11 @@ class SponsorshipQuerySet(QuerySet):
     def in_progress(self):
         status = [self.model.APPLIED, self.model.APPROVED]
         return self.filter(status__in=status)
+
+    def visible_to(self, user):
+        contacts = user.sponsorcontact_set.values_list('sponsor_id', flat=True)
+        status = [self.model.APPLIED, self.model.APPROVED, self.model.FINALIZED]
+        return self.filter(
+            Q(submited_by=user) | Q(sponsor_id__in=Subquery(contacts)),
+            status__in=status,
+        )
