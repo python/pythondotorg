9dc7cbe772e93225179dbf21c0497a624f38e06a
berin <bernardoxhc@gmail.com>
2021-10-07 13:15:59 -0300

    Update logo placement endpoint to read data from database (#1855)
    * Update logo placement endpoint to read data from database
    
    * Remove deprecated pypi csv
    
    * Simplify assertions
    
    * Remove TODO note

--- sponsors/tests/test_api.py
+++ sponsors/tests/test_api.py
@@ -1,10 +1,11 @@
-from model_bakery import baker
-
 from django.contrib.auth.models import Permission
 from django.urls import reverse_lazy
-
+from model_bakery import baker
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
+
+from sponsors.models import Sponsor
+from sponsors.enums import LogoPlacementChoices, PublisherChoices
 
 class LogoPlacementeAPIListTests(APITestCase):
     url = reverse_lazy("logo_placement_list")
@@ -15,23 +16,30 @@ class LogoPlacementeAPIListTests(APITestCase):
         self.permission = Permission.objects.get(name='Can access sponsor placement API')
         self.user.user_permissions.add(self.permission)
         self.authorization = f'Token {token.key}'
+        self.sponsors = baker.make(Sponsor, _create_files=True, _quantity=2)
+
+    def tearDown(self):
+        for sponsor in Sponsor.objects.all():
+            if sponsor.web_logo:
+                sponsor.web_logo.delete()
+            if sponsor.print_logo:
+                sponsor.print_logo.delete()
 
     def test_list_logo_placement_as_expected(self):
+        sp1, sp2 = baker.make_recipe("sponsors.tests.finalized_sponsorship", sponsor=iter(self.sponsors), _quantity=2)
+        baker.make_recipe("sponsors.tests.logo_at_download_feature", sponsor_benefit__sponsorship=sp1)
+        baker.make_recipe("sponsors.tests.logo_at_sponsors_feature", sponsor_benefit__sponsorship=sp1)
+        baker.make_recipe("sponsors.tests.logo_at_sponsors_feature", sponsor_benefit__sponsorship=sp2)
+
         response = self.client.get(self.url, HTTP_AUTHORIZATION=self.authorization)
         data = response.json()
-        expected_keys = set([
-            "publisher",
-            "sponsor",
-            "description",
-            "logo",
-            "start_date",
-            "end_date",
-            "sponsor_url",
-            "flight"
-        ])
 
         self.assertEqual(200, response.status_code)
-        self.assertEqual(expected_keys, set(data[0]))
+        self.assertEqual(3, len(data))
+        self.assertEqual(2, len([p for p in data if p["flight"] == LogoPlacementChoices.SPONSORS_PAGE.value]))
+        self.assertEqual(1, len([p for p in data if p["flight"] == LogoPlacementChoices.DOWNLOAD_PAGE.value]))
+        self.assertEqual(2, len([p for p in data if p["sponsor"] == self.sponsors[0].name]))
+        self.assertEqual(1, len([p for p in data if p["sponsor"] == self.sponsors[1].name]))
 
     def test_invalid_token(self):
         Token.objects.all().delete()
