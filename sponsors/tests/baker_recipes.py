d603c8a50798a8537742e78d16f409ea2fa834f6
Bernardo Fontes <bernardoxhc@gmail.com>
2022-03-03 17:58:34 -0300

    Explicitly set up end date on contract recipes

--- sponsors/tests/baker_recipes.py
+++ sponsors/tests/baker_recipes.py
@@ -7,11 +7,13 @@ from sponsors.models.enums import LogoPlacementChoices, PublisherChoices
 
 today = date.today()
 two_days = timedelta(days=2)
+thirty_days = timedelta(days=30)
 
 empty_contract = Recipe(
     Contract,
     sponsorship__sponsor__name="Sponsor",
     sponsorship__start_date=today,
+    sponsorship__end_date=today + thirty_days,
     benefits_list="",
     legal_clauses="",
 )
@@ -20,6 +22,7 @@ awaiting_signature_contract = Recipe(
     Contract,
     sponsorship__sponsor__name="Awaiting Sponsor",
     sponsorship__start_date=today,
+    sponsorship__end_date=today + thirty_days,
     benefits_list="- benefit 1",
     legal_clauses="",
     status=Contract.AWAITING_SIGNATURE,
