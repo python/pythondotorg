68039b0b5ad200786f56a270326f00c247f6e893
Anirudh <hegdeanirudh2003@gmail.com>
2023-07-18 07:38:11 +0530

    Updated init file

--- users/__init__.py
+++ users/__init__.py
@@ -1,1 +1,13 @@
+r"""
+Python is an easy to learn, powerful programming language. 
+It has efficient high-level data structures and a simple but 
+effective approach to object-oriented programming. Python’s 
+elegant syntax and dynamic typing, together with its interpreted 
+nature, make it an ideal language for scripting and rapid application 
+development in many areas on most platforms.
+
+The Python interpreter is easily extended with new functions and data types 
+implemented in C or C++ (or other languages callable from C). Python is also 
+suitable as an extension language for customizable applications.
+"""
 default_app_config = 'users.apps.UsersAppConfig'
