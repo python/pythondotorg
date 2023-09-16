0403ca15e685a69f98e23ff0b4c6029bc160132d
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-09-17 00:45:15 +0300

    Prioritise 64-bit downloads over 32-bit

--- downloads/templatetags/download_tags.py
+++ downloads/templatetags/download_tags.py
@@ -14,3 +14,21 @@ def has_sigstore_materials(files):
         f.sigstore_bundle_file or f.sigstore_cert_file or f.sigstore_signature_file
         for f in files
     )
+
+
+@register.filter
+def prioritise_64bit_over_32bit(files):
+    if not files:
+        return files
+
+    # Put 64-bit files before 32-bit
+    new = []
+    previous = files[0]
+    for i, current in enumerate(files):
+        if i >= 1 and '(64-bit)' in current.name and '(32-bit)' in previous.name:
+            new.insert(-1, current)
+        else:
+            new.append(current)
+        previous = current
+
+    return new
