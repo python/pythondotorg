ff81a7fa4ce36bb8d203ca907344877a91a1f899
Ee Durbin <ewdurbin@gmail.com>
2023-12-19 16:14:21 -0500

    use renewal template if applicable

--- sponsors/tests/test_contracts.py
+++ sponsors/tests/test_contracts.py
@@ -12,22 +12,27 @@ from sponsors.contracts import render_contract_to_docx_response
 class TestRenderContract(TestCase):
     def setUp(self):
         self.contract = baker.make_recipe("sponsors.tests.empty_contract", sponsorship__start_date=date.today())
-        self.context = {
-            "contract": self.contract,
-            "start_date": self.contract.sponsorship.start_date,
-            "start_day_english_suffix": format(self.contract.sponsorship.start_date, "S"),
-            "sponsor": self.contract.sponsorship.sponsor,
-            "sponsorship": self.contract.sponsorship,
-            "benefits": [],
-            "legal_clauses": [],
-        }
 
     # DOCX unit test
     def test_render_response_with_docx_attachment(self):
         request = Mock(HttpRequest)
+        self.contract.sponsorship.renewal = False
         response = render_contract_to_docx_response(request, self.contract)
 
         self.assertEqual(response.get("Content-Disposition"), "attachment; filename=sponsorship-contract-Sponsor.docx")
+        self.assertEqual(
+            response.get("Content-Type"),
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        )
+
+
+    # DOCX unit test
+    def test_render_renewal_response_with_docx_attachment(self):
+        request = Mock(HttpRequest)
+        self.contract.sponsorship.renewal = True
+        response = render_contract_to_docx_response(request, self.contract)
+
+        self.assertEqual(response.get("Content-Disposition"), "attachment; filename=sponsorship-renewal-Sponsor.docx")
         self.assertEqual(
             response.get("Content-Type"),
             "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
