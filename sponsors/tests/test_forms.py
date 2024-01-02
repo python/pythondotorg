56d2c8562f3c60143f179bbdb864ebd6c6a3a612
Jessie <70440141+jessiebelle@users.noreply.github.com>
2024-01-02 15:47:57 +0200

    Fix for SVG upload in sponsorship application (#2352)
    * changing imagefield to file field on sponsorupdateform and sponsorshipapplicationform. adding django file validator to these two and the sponsor model
    
    * allow png upload
    
    * adding test to assert that svg can be uploaded
    
    * fix migration

--- sponsors/tests/test_forms.py
+++ sponsors/tests/test_forms.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+from django.core.files.uploadedfile import SimpleUploadedFile
 from model_bakery import baker
 
 from django.conf import settings
@@ -437,6 +440,21 @@ class SponsorshipApplicationFormTests(TestCase):
         self.assertFalse(form.user_with_previous_sponsors)
         self.assertEqual(sponsor.landing_page_url, "https://companyx.com")
         self.assertEqual(sponsor.twitter_handle, "@companyx")
+
+    def test_create_sponsor_with_svg_for_print_logo(
+            self,
+    ):
+        tick_svg = Path(settings.STATICFILES_DIRS[0]) / "img"/"sponsors"/"tick.svg"
+        with tick_svg.open("rb") as fd:
+            uploaded_svg = SimpleUploadedFile("tick.svg", fd.read())
+        self.files["print_logo"] = uploaded_svg
+
+        form = SponsorshipApplicationForm(self.data, self.files)
+        self.assertTrue(form.is_valid(), form.errors)
+
+        sponsor = form.save()
+
+        self.assertTrue(sponsor.print_logo)
 
     def test_use_previous_user_sponsor(self):
         contact = baker.make(SponsorContact, user__email="foo@foo.com")
