eb23cb7cd668726fe1961242c703be1749370330
Kiran Kolte <62833568+kskolte2020@users.noreply.github.com>
2024-12-03 10:00:54 +0530

    Issue 2662 Update urls.py
    Redirect pythoninsider.blogspot.com to blog.python.org and other permalinks as well.

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,6 +1,9 @@
 from . import views
-from django.urls import path
+from django.urls import path, re_path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', views.BlogHome.as_view(), name='blog'),
+    # Redirect all requests from pythoninsider.blogspot.com to blog.python.org
+    re_path(r'^(?P<path>.*)$', RedirectView.as_view(url='https://blog.python.org/%(path)s', permanent=True)),
 ]
