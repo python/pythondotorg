d5b418d7298be4667e5d4c8554aee8c5fe2056e4
Ee Durbin <ewdurbin@gmail.com>
2024-12-11 16:02:45 -0500

    remove vendored retina.js, retire @2x logos to default

--- static/js/plugins.js
+++ static/js/plugins.js
@@ -25,30 +25,6 @@ if(!key){result[name]=converted(cookie);}}
 return result;};config.defaults={};$.removeCookie=function(key,options){if($.cookie(key)!==undefined){$.cookie(key,'',$.extend(options,{expires:-1}));return true;}
 return false;};}));
 
-
-/*! Retina.js
- * https://github.com/imulus/retinajs/blob/master/src/retina.js
- * Copyright (C) 2012 Ben Atkin
- * MIT License.
- */
-(function(){var root=(typeof exports=='undefined'?window:exports);var config={check_mime_type:true};root.Retina=Retina;function Retina(){}
-Retina.configure=function(options){if(options===null)options={};for(var prop in options)config[prop]=options[prop];};Retina.init=function(context){if(context===null)context=root;var existing_onload=context.onload||new Function;context.onload=function(){var images=document.getElementsByTagName("img"),retinaImages=[],i,image;for(i=0;i<images.length;i++){image=images[i];retinaImages.push(new RetinaImage(image));}
-existing_onload();}};Retina.isRetina=function(){var mediaQuery="(-webkit-min-device-pixel-ratio: 1.5),\
-                      (min--moz-device-pixel-ratio: 1.5),\
-                      (-o-min-device-pixel-ratio: 3/2),\
-                      (min-resolution: 1.5dppx)";if(root.devicePixelRatio>1)
-return true;if(root.matchMedia&&root.matchMedia(mediaQuery).matches)
-return true;return false;};root.RetinaImagePath=RetinaImagePath;function RetinaImagePath(path){this.path=path;this.at_2x_path=path.replace(/\.\w+$/,function(match){return"@2x"+match;});}
-RetinaImagePath.confirmed_paths=[];RetinaImagePath.prototype.is_external=function(){return!!(this.path.match(/^https?\:/i)&&!this.path.match('//'+document.domain));}
-RetinaImagePath.prototype.check_2x_variant=function(callback){var http,that=this;if(this.is_external()){return callback(false);}else if(this.at_2x_path in RetinaImagePath.confirmed_paths){return callback(true);}else{http=new XMLHttpRequest;http.open('HEAD',this.at_2x_path);http.onreadystatechange=function(){if(http.readyState!=4){return callback(false);}
-if(http.status>=200&&http.status<=399){if(config.check_mime_type){var type=http.getResponseHeader('Content-Type');if(type===null||!type.match(/^image/i)){return callback(false);}}
-RetinaImagePath.confirmed_paths.push(that.at_2x_path);return callback(true);}else{return callback(false);}}
-http.send();}}
-function RetinaImage(el){this.el=el;this.path=new RetinaImagePath(this.el.getAttribute('src'));var that=this;this.path.check_2x_variant(function(hasVariant){if(hasVariant)that.swap();});}
-root.RetinaImage=RetinaImage;RetinaImage.prototype.swap=function(path){if(typeof path=='undefined')path=this.path.at_2x_path;var that=this;function load(){if(!that.el.complete){setTimeout(load,5);}else{that.el.setAttribute('width',that.el.offsetWidth);that.el.setAttribute('height',that.el.offsetHeight);that.el.setAttribute('src',path);}}
-load();}
-if(Retina.isRetina()){Retina.init(root);}})();
-
 /*
  * jQuery FlexSlider v2.1
  * http://www.woothemes.com/flexslider/
