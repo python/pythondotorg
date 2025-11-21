0a09306e5cd96b98cc2df7553ce22c3828f8477e
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2025-11-21 18:18:55 +0200

    Use first() instead of qs[0]

--- downloads/managers.py
+++ downloads/managers.py
@@ -48,29 +48,13 @@ class ReleaseQuerySet(QuerySet):
 
 class ReleaseManager(Manager.from_queryset(ReleaseQuerySet)):
     def latest_python2(self):
-        qs = self.get_queryset().latest_python2()
-        if qs:
-            return qs[0]
-        else:
-            return None
+        return self.get_queryset().latest_python2().first()
 
     def latest_python3(self):
-        qs = self.get_queryset().latest_python3()
-        if qs:
-            return qs[0]
-        else:
-            return None
+        return self.get_queryset().latest_python3().first()
 
     def latest_python3x(self, minor_version: int):
-        qs = self.get_queryset().latest_python3x(minor_version)
-        if qs:
-            return qs[0]
-        else:
-            return None
+        return self.get_queryset().latest_python3x(minor_version).first()
 
     def latest_pymanager(self):
-        qs = self.get_queryset().latest_pymanager()
-        if qs:
-            return qs[0]
-        else:
-            return None
+        return self.get_queryset().latest_pymanager().first()
