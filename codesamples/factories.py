71396b7131412d03370bc263d406ab72c6fa5e28
ArminHaberl <80152869+ArminHaberl@users.noreply.github.com>
2025-11-26 10:06:07 +0100

    Remove unnecessary print statement in fib function

--- codesamples/factories.py
+++ codesamples/factories.py
@@ -126,7 +126,6 @@ def initial_data():
             ...     while a &lt; n:
             ...         print(a, end=' ')
             ...         a, b = b, a+b
-            ...     print()
             ...
             >>> fib(1000)
             <span class=\"output\">0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610</span>
