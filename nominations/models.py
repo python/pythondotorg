e3eb1fd4b89f17c4d4c8590011aca5700a53f1d5
Jacob Coffee <jacob@z7x.org>
2026-02-05 22:15:46 -0600

    unused import

--- nominations/models.py
+++ nominations/models.py
@@ -1,7 +1,6 @@
 import datetime
 
 from django.conf import settings
-from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
