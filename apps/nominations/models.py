b0b4526867202f2349d6af97cea9e35770d7ee91
ParzivalT1 <diepnguyenhong834@gmail.com>
2026-08-18 23:08:12 +0700

    Allow nominators to view their nominees

--- apps/nominations/models.py
+++ apps/nominations/models.py
@@ -245,10 +245,13 @@ class Nominee(models.Model):
         if self.accepted and self.approved and not self.election.nominations_open:
             return True
 
-        if user is None:
+        if user is None or not user.is_authenticated:
             return False
 
-        return bool(user.is_staff or user == self.user)
+        if user.is_staff or user == self.user:
+            return True
+
+        return self.nominations.filter(election=self.election, nominator=user).exists()
 
 
 class Nomination(models.Model):
